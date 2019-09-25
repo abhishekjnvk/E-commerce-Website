a9e6d25701f8db2a0f368c59608c93108d26a85a
Abhishek <abhishekkumarjnvk@gmail.com>
2019-09-26 01:07:07 +0530

    navigation Bar

---
+++ include/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary site-header sticky-top">
+  <a class="navbar-brand" href="#"><img src="images/logo.gif" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link fa fa-home" href="index.php"> Home <!-- <span class="sr-only">(current)</span> --></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link fa fa-globe" href="all.php"> Products <!-- <span class="sr-only">(current)</span> --></a>
+      </li>
+
+      <li class="nav-item active">
+        <a class="nav-link fa fa-list-ul" href="list.php"> Categories <!-- <span class="sr-only">(current)</span> --></a>
+      </li>
+
+      <li class="nav-item active">
+        <a class="nav-link fa fa-address-card" href="contact_us.php"> Contact Us <!-- <span class="sr-only">(current)</span> --></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0 fa fa-search" type="submit" > </button>
+    </form>
+  </div>
+</nav>
+<ul class="nav justify-content-end">
+  <li class="nav-item active">
+  Welcome  <a class="nav-link fa fa-user-circle" href="my_account.php"> {Username} <!-- <span class="sr-only">(current)</span> --></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link fa fa-shopping-cart" href="cart.php"> Cart</a>
+  </li>
+</ul>
+  <!-- 
+<div class="row">
+<div class="col-lg-2" style="height: 100vh; background-color: #a7d1f0;" > hi</div>
+<div class="col-lg-10">
+
+</div>
+</div> -->
---
+++ my_account.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://use.fontawesome.com/your-embed-code.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <script src="https://use.fontawesome.com/ba650291e1.js"></script> -->
+
+
+<script src="https://use.fontawesome.com/99cc9f4c78.js"></script>
+
+
+    <title>Online Shop</title>
+  </head>
+  <body class="bg-dark">
+    <div style="width: 98%; height: 100vh; margin: auto;" class="bg-light">
+    <?php include('include/navbar.php') ?>
+
+
+
+ 
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
